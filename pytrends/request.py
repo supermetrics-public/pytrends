2501f5985cfee42726a999c4db213fe3335b5e23
denis-zavialkin <denis.zavialkin@supermetrics.com>
2021-09-14 08:50:23 +0300

    Retry sub-dataframe if no geoCode in interest_by_region

--- pytrends/request.py
+++ pytrends/request.py
@@ -298,8 +298,13 @@ class TrendReq(object):
             return df
 
         # rename the column with the search keyword
-        df = df[['geoName', 'geoCode', 'value']].set_index(
-            ['geoName']).sort_index()
+        try:
+            df = df[['geoName', 'geoCode', 'value']].set_index(
+                ['geoName']).sort_index()
+        except KeyError:
+            df = df[['geoName', 'value']].set_index(
+                ['geoName']).sort_index()
+
         # split list columns into separate ones, remove brackets and split on comma
         result_df = df['value'].apply(lambda x: pd.Series(
             str(x).replace('[', '').replace(']', '').split(',')))
